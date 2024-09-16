6aabb0f7e451b4b11cbf8f6c2029d8f2b7613805
talhaatconcora <talhayousuf.work@gmail.com>
2024-09-17 02:32:23 +0500

    Deployment Using User Data Script

--- demo-nodejs-app/app.js
+++ demo-nodejs-app/app.js
@@ -1,9 +1,22 @@
-// app.js
 const http = require("http");
 const port = 3000;
 
+const routes = {
+  "/": "Hello world form a demo Node.js app deployed with AWS",
+  "/status": "The is the status page!",
+  "/info": "This is the info page!",
+  "/health": "Health check passed!",
+};
+
 const requestHandler = (request, response) => {
-  response.end("Hello, this is a demo Node.js app deployed with CodeDeploy!");
+  const url = request.url;
+
+  if (routes[url]) {
+    response.end(routes[url]);
+  } else {
+    response.statusCode = 404;
+    response.end("Page not found!");
+  }
 };
 
 const server = http.createServer(requestHandler);
